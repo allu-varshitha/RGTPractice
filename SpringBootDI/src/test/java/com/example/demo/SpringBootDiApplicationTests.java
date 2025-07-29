ec991a9e0dde3f869af54061c33c30cf1b8fba6b
VarshithaApp <varsh123>
2025-07-29 22:23:54 +0530

    feat 28/07/25 Sprinboot DI

--- SpringBootDI/src/test/java/com/example/demo/SpringBootDiApplicationTests.java
+++ SpringBootDI/src/test/java/com/example/demo/SpringBootDiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.practice;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootDemoApplicationTests {
+class SpringBootDiApplicationTests {
 
 	@Test
 	void contextLoads() {
