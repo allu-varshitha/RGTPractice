047ed66f49dbf797f31f8955da2f970497f549ef
VarshithaApp <varsh123>
2025-07-29 22:07:08 +0530

    feat 28/07/25 Lombok annotations

---
+++ Lombok/src/main/java/com/practice/Student.java
@@ -0,0 +1,31 @@
+package com.practice;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//@Setter
+//@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+//@ToString
+
+
+@Data
+public class Student {
+
+	private String name;
+    private int roll;
+	private int age;
+	
+	// lombok provide us annoations
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ Lombok/src/main/java/com/practice/TestStudent.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+  
+		Student s1=new Student("Varshitha",513,21);
+		System.out.println(s1);
+		
+	}
+
+}
