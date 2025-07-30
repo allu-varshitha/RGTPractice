a8b4f0673ce4870941f5b21d0bec04da961de249
VarshithaApp <varsh123>
2025-07-30 11:46:20 +0530

    feat 30/07/25 using crud repository

--- CurdExample/src/main/java/com/practice/CurdExampleApplication.java
+++ CurdExample/src/main/java/com/practice/CurdExampleApplication.java
@@ -1,5 +1,8 @@
 package com.practice;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -23,9 +26,71 @@ public class CurdExampleApplication {
 		s1.setEmail("abc@123");
 		
 		//save() is used to insert data into database
-		st.save(s1);
-		System.out.println("data inserted successfully");
+//		st.save(s1);
+//		System.out.println("data inserted successfully");
+		StudentEntity s2=new StudentEntity();
+		s2.setId(2);
+		s2.setName("Kiran reddy");
+		s2.setPhnno(664999);
+		s2.setRollno(609);
+		s2.setEmail("xetr@123");
+		//save() is used to insert data into database
+//		st.save(s2);
+//		<------- saveAll()------->
 		
+		StudentEntity s3=new StudentEntity(3, "janu",23345 ,"ade@123",522);
+		StudentEntity s4=new StudentEntity(4, "maha", 4321, "ser@123", 567);
+//		st.saveAll(Arrays.asList(s3,s4));
+		
+		
+		//<----- findById()------> to fetch one record  based on primary key id
+//		int id=2;
+//		Optional<StudentEntity>se=st.findById(id);
+//		if(se.isPresent()) {
+//			System.out.println(se.get());
+//		}else {
+//			System.out.println("Student with"+" "+id+" "+"not available");
+//		}
+		
+		
+		//-------findAllById------->to fetch multiple records  by a list of ids
+		
+		
+//		Iterable<StudentEntity>stid=st.findAllById(Arrays.asList(3));	
+//		for(StudentEntity ste:stid) {
+//			System.out.println(ste);
+//		}
+//		System.out.println("all feteched ");
+//		
+//		
+		
+		//count()
+		
+//		System.out.println(st.count());
+		
+	
+		//deletebyid()---deletes the record based on id
+//		
+//		int id=4;
+//		if(st.existsById(id)) {
+//			st.deleteById(id);
+//			System.out.println("Record deleted");
+//		}else {
+//			System.out.println("no stdent with these id: "+id);
+//		}
+		
+		
+	// delete() --delete a record based on its object
+		
+//		st.delete(s3);
+//		System.out.println("deleted student based on entity");
+//		
+		
+		
+		
+		//deleteAllbyId()----delete multiple records
+		
+		st.deleteAllById(Arrays.asList(3,4));
 	}
 
 }
--- CurdExample/src/main/java/com/practice/entity/StudentEntity.java
+++ CurdExample/src/main/java/com/practice/entity/StudentEntity.java
@@ -3,16 +3,18 @@ package com.practice.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
  
 @Entity // This annotation defines that a class can be mapped to a table
 @Data   // which is equals to getters,setters,tostring,constructor
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name="Student")
 public class StudentEntity {// table
 	
 	@Id
-	
 	private int id;// represents primary key  for the column
 	private String name;//columns
 	private long phnno;
